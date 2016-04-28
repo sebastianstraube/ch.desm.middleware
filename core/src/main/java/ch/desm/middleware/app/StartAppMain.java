960fbc485323d95868f18232fe1415639c2bfc74
Maximilian Haupt <mail@maximilianhaupt.com>
2016-04-29 00:07:43 +0200

    Rename RunApp to Middleware and introduce MiddlewareConfig.

--- core/src/main/java/ch/desm/middleware/app/StartAppMain.java
+++ core/src/main/java/ch/desm/middleware/app/StartAppMain.java
@@ -1,48 +1,50 @@
 package ch.desm.middleware.app;
 
-import org.apache.commons.lang3.SystemUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class StartAppMain {
 
     private static Logger LOGGER = Logger.getLogger(StartAppMain.class);
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         LOGGER.log(Level.INFO, "initialise middleware...");
-        LOGGER.log(Level.INFO, "starting directory: "+ System.getProperty("user.dir"));
+        LOGGER.log(Level.INFO, "starting directory: " + System.getProperty("user.dir"));
 
-        RunApp main = new RunApp();
-
+        final MiddlewareConfig config = new MiddlewareConfig();
         //String host, String port, String websocketContextPath, String serverEndpointContextPath, String jettyPath
-        if(args.length >0 && args.length <= 4){
-            main.setConfiguration(args[0], args[1], args[2], args[3], args[4]);
-        } else {
-            main.setConfiguration("192.168.1.20", "8070", "/websocket", "/gui", System.getProperty("user.dir"));
+        if (args.length > 0 && args.length <= 4) {
+            config.setManagementHost(args[0]);
+            config.setManagementPort(Integer.parseInt(args[1]));
+            config.setTyrusWebsocketContextPath(args[2]);
+            config.setServerEndpointContextPath(args[3]);
+            config.setServerJettyPath(args[4]);
         }
 
-        if(main.isConfigured()){
-            LOGGER.log(Level.INFO, "starting middleware...");
-            main.start();
-        }else{
-            LOGGER.log(Level.ERROR, "configuration error: " + Arrays.asList(args));
-        }
+        LOGGER.log(Level.INFO, "Starting middleware with configuration: " + config);
+        final Middleware middleware = new Middleware(config);
+
+        LOGGER.log(Level.INFO, "starting middleware...");
+        middleware.start();
 
         LOGGER.log(Level.INFO, "Press any key to close middleware...");
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
+        waitForAnyKey();
+
+        LOGGER.log(Level.INFO, "shutting down middleware...");
+        System.exit(0);
+    }
+
+    private static void waitForAnyKey() {
+        final InputStreamReader isr = new InputStreamReader(System.in);
+        final BufferedReader br = new BufferedReader(isr);
         try {
             br.read();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        LOGGER.log(Level.INFO, "shutting down middleware...");
-        System.exit(0);
     }
 }
