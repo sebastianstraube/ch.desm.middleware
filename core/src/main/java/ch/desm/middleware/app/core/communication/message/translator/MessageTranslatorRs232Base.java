e1e1f03b1057db8671fdcd39f69348e05cc5753f
Maximilian Haupt <mail@maximilianhaupt.com>
2016-04-30 11:02:36 +0200

    Remove unused class MessageTranslatorRs232Base.

--- core/src/main/java/ch/desm/middleware/app/core/communication/message/translator/MessageTranslatorRs232Base.java
+++
@@ -1,8 +0,0 @@
-package ch.desm.middleware.app.core.communication.message.translator;
-
-
-public abstract class MessageTranslatorRs232Base extends MessageTranslatorMiddlewareBase {
-	
-	public abstract String translateToCommonMiddlewareMessage(String payload, String topic);
-
-}
