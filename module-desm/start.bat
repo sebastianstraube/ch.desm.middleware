12204a6e6856efcc83b463c7d37e2ad0548f1516
Sebastian Str <github@straube.ch>
2016-06-12 20:24:49 +0200

    create start for win env.

---
+++ module-desm/start.bat
@@ -0,0 +1,2 @@
+echo "Let's start DESM environment..."
+java -jar target\module-desm-0.1-jar-with-dependencies.jar "C:\connectx\connectx\core\config_server.json" "C:\connectx\connectx\module-desm\config_desm.json"
